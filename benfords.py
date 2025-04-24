dad06345df1273578ddfc4af83ead5738d92f7dc
Thomas Barbato <t.barbato@ufl.edu>
2025-04-23 21:33:23 -0400

    Attempt to make get_digit_frequencies() applicable for a number system with any base

--- benfords.py
+++ benfords.py
@@ -1,12 +1,12 @@
-def get_digit_frequencies(filename):
+def get_digit_frequencies(filename) -> dict:
+    """Returns a dictionary with the leading digits as keys and their frequencies as values."""
     file = open(filename, 'r')
     str_list = []
-    while True:
-        line = file.readline()
-        if not line:
-            break
-        str_list.append(line)
-    ret_list = [0] * 10
+    while str_list := file.readlines():
+        pass
+    ret_dict = dict()
     for line in str_list:
-        ret_list[int(line[0])] += 1
-    return ret_list
+        if not ret_dict[line[0]]:
+            ret_dict[line[0]] = 0
+        ret_dict[line[0]] += 1
+    return ret_dict
