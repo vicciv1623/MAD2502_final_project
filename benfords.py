083845e2e030108c9c80201bad0b7863613f37be
Thomas Barbato <t.barbato@ufl.edu>
2025-04-23 21:43:52 -0400

    Wrote get_digit_proportions() and edited Notes_and_Ideas.txt

--- benfords.py
+++ benfords.py
@@ -10,3 +10,12 @@ def get_digit_frequencies(filename) -> dict:
             ret_dict[line[0]] = 0
         ret_dict[line[0]] += 1
     return ret_dict
+
+def get_digit_proportions(filename) -> dict:
+    """Returns a dictionary with the leading digits as keys and their proportions as values."""
+    frequencies = get_digit_frequencies(filename)
+    total = sum(frequencies.values())
+    ret_dict = dict()
+    for key, value in frequencies.items():
+        ret_dict[key] = value / total
+    return ret_dict
