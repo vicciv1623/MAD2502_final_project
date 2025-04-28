d0c00e7bb6d853190984b1022609a91f9faaf183
HAHAHAHAHHA21 <alexbox365@live.com>
2025-04-28 01:24:42 -0400

    huge update since i figured out what to do with zipf's law properly

--- text_extraction.py
+++ text_extraction.py
@@ -1,16 +1,7 @@
 import re
-import scipy.stats as stats
 
-def word_frequencies(file_name : str):
-    """Finds the frequencies of each word in a file
-
-    Parameters:
-        file_name (str): The file
-
-    Returns:
-        list[tuple(str, int)]: A list of word-frequency tuples
-
-    """
+def word_freq_pairs(file_name : str):
+    """Finds the frequencies of each word in a file"""
     with open(file_name, 'r') as f:
         d = dict()
         text = re.sub("[^a-zA-Z ]", '', f.read())
@@ -20,26 +11,43 @@ def word_frequencies(file_name : str):
            else: d[line] = 1
 
 
-    return list(d)
+    return d
 
-def word_proportions(file_name : str):
-    """Finds the proportion of each word in a text file
+def word_frequencies(word_freq_pairs : dict) -> np.ndarray:
+    """The numerical word frequencies of each word in a file"""
+    freqs = np.array(sorted(word_freq_pairs.values(), reverse = True))
+    return freqs
 
-    Parameters: file_name (str): The file
+def zipf_expected_freqs(file_name: str) -> np.ndarray:
+    total_words = word_count(file_name)
+    word_freqs = word_freq_pairs(file_name)
+    unique_words = len(word_frequencies(word_freqs))
+    ranks = np.arange(1, unique_words + 1)
+    const = np.sum(1.0 / ranks)
+    norm = unique_words / const #need to normalize for zipf
+    return norm / ranks
 
-    Returns: list[tuple(str, float)]: A list of word-proportion tuples
-
-    """
-    word_freqs = dict(word_frequencies(file_name))
+def word_count(file_name: str) -> float:
     with open(file_name, 'r') as f:
         text = re.sub("[^a-zA-Z ]", '', f.read())
         count = 0
         for word in text.split():
             count += 1
-        for value in word_freqs.values():
-            value = value / count
 
-    return list(word_freqs)
+    return count
+
+def plot_zipf(text_file: str):
+    empirical_freqs = word_frequencies(word_freq_pairs(text_file))
+    expected_freqs = zipf_expected_freqs(text_file)
+    ranks = np.arange(1, len(empirical_freqs) + 1)
+    plt.loglog(ranks, empirical_freqs, color = 'r', label = "Actual")
+    plt.loglog(ranks, expected_freqs, color = 'b', label = "Expected")
+    plt.xlabel("Log Rank")
+    plt.ylabel("Log Frequency")
+    plt.legend()
+    plt.title(f"Zipf's Law with {text_file[:-4]}")
+    plt.grid(True, which = "both", ls = "--", alpha = 0.5)
+    plt.show()
 
 
 
